a969419ff7cb01cebf3fddbd7e15b0ee96271d3e
Iuliia El Sammak <juliag875@gmail.com>
2022-03-29 17:08:46 -0400

    test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
+import HostContainer from './HostContainer';
+import Navbar from './Navbar';
+import MyRental from './MyRental';
 
 function App() {
   const [hosts, setHosts] = useState([]);
@@ -13,11 +16,11 @@ function App() {
  
   return (
     <div className="App">
-      <NavBar />
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<App />} />
         <Route exact path="/rentals" element={<MyRental />} />
-        <Route path="/host" element={<Host hosts = {hosts} />} />
+        <Route path="/host" element={<HostContainer hosts = {hosts} />} />
       </Routes> 
     </div>
   );
