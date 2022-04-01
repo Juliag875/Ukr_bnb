4af2a82b44ffb1a4aa7ac965e9f701887690d045
Iuliia El Sammak <juliag875@gmail.com>
2022-04-01 01:41:22 -0400

    login

--- client/src/components/HostForm.js
+++ client/src/components/HostForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 function HostForm() {
   const [hosts, setHosts] = useState([]);
@@ -19,7 +19,7 @@ function HostForm() {
     email: "",
   });
 
-  const history = useHistory();
+  // const history = useHistory();
 
   function handleChange(event) {
     setFormData({
@@ -46,15 +46,15 @@ function HostForm() {
       .then((r) => r.json())
       .then((newCard) => handleAddCard(newCard));
         setFormData({ name: "", email: "" });
-        console.log(newCard);
-        history.push('/search'
+        window.location.href = "https://www.airbnb.com/for-airbnb-org/host?_ga=2.49331118.905296572.1648657862-745576770.1643075090"
+        // history.push('/search
       // "https://www.airbnb.com/for-airbnb-org/host?_ga=2.49331118.905296572.1648657862-745576770.1643075090"
-    );
+    // );
   }
 
   return (
     <div className="formcontainer">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} href="https://www.airbnb.com/for-airbnb-org/host?_ga=2.246354828.905296572.1648657862-745576770.1643075090">
         <input
           type="text"
           name="name"
@@ -63,7 +63,6 @@ function HostForm() {
           placeholder="Add name..."
           className="input-text"
         />
-        <br></br>
         <br></br>
         <input
           type="email"
@@ -74,9 +73,8 @@ function HostForm() {
           className="input-text"
         />
         <br></br>
-        <br></br>
-        <button
-          style={{ backgroundColor: "red" }}
+        <button 
+          style={{ backgroundColor: "" }}
           type="submit"
           name="submit"
           value="Add New Rental"
